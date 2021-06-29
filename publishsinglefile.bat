37d16ad4d4d4cc1c24d942d638baa387fbbe1476
rfuzzo <r.fuzzo@gmail.com>
2021-06-29 17:25:44 +0200

    git submodules

--- publishsinglefile.bat
+++ publishsinglefile.bat
@@ -4,3 +4,4 @@
 dotnet publish .\WolvenManager.UI\WolvenManager.UI.csproj -o publish\singlefile -p:PublishSingleFile=true --no-self-contained -r win-x64 -c Release -f net5.0-windows
 
 echo "The program has completed"
+@pause
